2f7b659cb7030f5c0f3f61eb6699d1a1add9a6d0
GOPIKRISHNA-M-A <gopikrishna6003@gmail.com>
2023-08-20 16:41:33 +0530

    baseURL

--- src/components/baseURL.js
+++ src/components/baseURL.js
@@ -1,5 +1,5 @@
 
-export const baseURL = 'http://localhost:4000'
-// export const baseURL = 'https://question-paper-api.onrender.com'
+// export const baseURL = 'http://localhost:4000'
+export const baseURL = 'https://question-paper-api.onrender.com'
 
 export default baseURL;
