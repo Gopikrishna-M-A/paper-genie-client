d8348469f54d8e9a761e7765bddc0653b3d545a0
GOPIKRISHNA-M-A <gopikrishna6003@gmail.com>
2023-09-02 11:18:06 +0530

    custom domain

--- src/components/baseURL.js
+++ src/components/baseURL.js
@@ -1,5 +1,5 @@
 
-// export const baseURL = 'http://localhost:4000'
-export const baseURL = 'https://paper-genie-api.onrender.com'
+export const baseURL = 'http://localhost:4000'
+// export const baseURL = 'https://api.papergenie.co.in'
 
 export default baseURL;
