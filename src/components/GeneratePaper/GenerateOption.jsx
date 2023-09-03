1e63a67cac8d4df58f2fb3c6b71992c8416374fd
GOPIKRISHNA-M-A <gopikrishna6003@gmail.com>
2023-09-03 20:43:53 +0530

    style changes

--- src/components/GeneratePaper/GenerateOption.jsx
+++ src/components/GeneratePaper/GenerateOption.jsx
@@ -1,32 +1,34 @@
 import React from 'react'
-import { Button } from "antd";
+import { Button, Typography } from "antd";
 import { Link } from 'react-router-dom';
+
+const { Title, Text } = Typography;
 
 export default function GenerateOption() {
   return (
     <div className="generate-opt-section">
     <div className="custom-paper-card">
-      <h2>Custom Paper Generation</h2>
-      <p>Create a paper with specific criteria:</p>
-      <ul>
-        <li>Choose the number of questions.</li>
-        <li>Select difficulty levels (easy, medium, difficult).</li>
-        <li>Select Cognitive levels (Knowledge, Comprehension, Application).</li>
-        <li>Specify section</li>
-        <li>Specify subject</li>
+      <Title level={1}>Custom Paper</Title>
+      {/* <Text type={'secondary'}>Create a paper with specific criteria:</Text> */}
+      <ul className='main'>
+        <li><Text>Choose the number of questions.</Text></li>
+        <li><Text>Select difficulty levels.</Text></li>
+        <li><Text>Select Cognitive levels.</Text></li>
+        <li><Text>Specify section</Text></li>
+        <li><Text>Specify subject</Text></li>
       </ul>
-      <Link to={"/Create-custom-paper"}><Button type='primary' size='large'>Generate Custom Paper</Button></Link>
+      <Link to={"/Create-custom-paper"}><Button block type='primary' size='large'>Generate Custom Paper</Button></Link>
     </div>
     
     <div className="random-paper-card">
-      <h2>Random Paper Generation</h2>
-      <p>Generate a paper with random questions:</p>
-      <ul>
-        <li>Specify the number of easy, medium, and hard questions needed.</li>
-        <li>Quickly create papers without specifying individual questions.</li>
-        <li>Specify subject</li>
+      <Title level={1}>Random Paper</Title>
+      {/* <Text type={'secondary'}>Generate a paper with random questions:</Text> */}
+      <ul className='main'>
+        <li><Text>Specify the number of easy, medium, and hard questions needed.</Text></li>
+        <li><Text>Quickly create papers without specifying individual questions.</Text></li>
+        <li><Text>Specify subject</Text></li>
       </ul>
-      <Link to={"/Create-random-paper"}><Button type='primary' size='large'>Generate Random Paper</Button></Link>
+      <Link to={"/Create-random-paper"}><Button block type='primary' size='large'>Generate Random Paper</Button></Link>
       
     </div>
   </div>
