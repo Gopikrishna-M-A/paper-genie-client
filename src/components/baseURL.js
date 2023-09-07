6db07f6c40cf548f065f88eca910b43802240910
GOPIKRISHNA-M-A <gopikrishna6003@gmail.com>
2023-09-07 20:12:36 +0530

    mobile img compressed

--- src/components/baseURL.js
+++ src/components/baseURL.js
@@ -1,5 +1,5 @@
 
 // export const baseURL = 'http://localhost:4000'
-export const baseURL = 'https://api.papergenie.co.in'
+export const baseURL = 'https://api.papergenie.co.in' 
 
 export default baseURL;
