935ea76c73302f643b989a0ff2688c9da457eb5a
GOPIKRISHNA-M-A <gopikrishna6003@gmail.com>
2023-09-07 18:40:01 +0530

    router update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home/Home";
 import GenerateOption from "./components/GeneratePaper/GenerateOption";
@@ -19,63 +19,57 @@ import axios from 'axios'
 import { message, ConfigProvider } from "antd";
 
 function App() {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
   
   const handleWarning = () => {
     message.warning('Question added successfully!');
   };
 
   useEffect(() => {
-      axios
-        .get(`${baseURL}/auth/check-auth`, { withCredentials: true })
-        .then((response) => {
-          if (response.data.isAuthenticated) {
-            setUser(response.data.user);
-          } else {
-            setUser(null);
-          }
-        })
-        .catch((error) => {
-          console.error("Error fetching user data:", error);
-        });
+    axios
+      .get(`${baseURL}/auth/check-auth`, { withCredentials: true })
+      .then((response) => {
+        if (response.data.isAuthenticated) {
+          setUser(response.data.user);
+        } else {
+          setUser(null);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching user data:", error);
+      });
   }, []);
 
-
-
   return (
-
     <ConfigProvider
-    theme={{
-      token: {
-        // Seed Token
-        colorPrimary: "#242527",
-        // Alias Token
-        colorBgContainer: "#ffffff",
-      },
-    }}
-  >
-    <Router>
-      <Navbar user={user} setUser={setUser} className="nav" ></Navbar>
-      <Routes >
-        <Route path="/*" element={<Home user={user} />} />
-        <Route path="/" element={<Home user={user} />} />
-        <Route path="/tutorial" element={<Tutorial />} />
-        <Route path="/Add-question" element={user ? <AddQuestion user={user} /> : <Login user={user}/>} />
-        <Route path="/Create" element={user ? <GenerateOption user={user} /> : <Login user={user}/>} />
-        <Route path="/Create-custom-paper" element={user ? <GeneratePaper user={user} /> : <Login user={user}/>} />
-        <Route path="/Create-random-paper" element={user ? <GenerateRandomPaper user={user} /> : <Login user={user}/>} />
-        <Route path="/view-questions" element={user ? <Questions user={user} /> : <Login user={user}/>}  />
-        <Route path="/question-paper" element={<QuestionPaper user={user} />} />
-        <Route path="/login" element={<Login user={user} setUser={setUser} />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/settings" element={<Settings/>} />
-      </Routes>
-      <Footer></Footer>
-
-    </Router>
-
+      theme={{
+        token: {
+          // Seed Token
+          colorPrimary: "#242527",
+          // Alias Token
+          colorBgContainer: "#ffffff",
+        },
+      }}
+    >
+      <Router>
+        <Navbar user={user} setUser={setUser} className="nav" />
+        <Routes>
+          <Route path="/" element={<Home user={user} />} />
+          <Route path="/tutorial" element={<Tutorial />} />
+          <Route path="/Add-question" element={user ? <AddQuestion user={user} /> : <Login user={user} />} />
+          <Route path="/Create" element={user ? <GenerateOption user={user} /> : <Login user={user} />} />
+          <Route path="/Create-custom-paper" element={user ? <GeneratePaper user={user} /> : <Login user={user} />} />
+          <Route path="/Create-random-paper" element={user ? <GenerateRandomPaper user={user} /> : <Login user={user} />} />
+          <Route path="/view-questions" element={user ? <Questions user={user} /> : <Login user={user} />} />
+          <Route path="/question-paper" element={<QuestionPaper user={user} />} />
+          <Route path="/login" element={<Login user={user} setUser={setUser} />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+        <Footer />
+      </Router>
     </ConfigProvider>
-  )
+  );
 }
 
 export default App;
